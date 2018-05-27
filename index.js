408400e67a66b59be8e61636af66f7e1ed76a0e5
Emil Ajdyna <2088208+emilos@users.noreply.github.com>
2018-05-27 22:22:05 +0200

    Add loader utils

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const { compile } = require('@buxlabs/html-engine')
+const { getOptions } = require('loader-utils')
 
-module.exports = function (source, options) {
+module.exports = function (source) {
   if (this.cacheable) this.cacheable()
+  const options = getOptions(this)
   const output = compile(source, options)
   return `export default ${output.toString()}`
 }
