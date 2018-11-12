5b0076648f8a427e384fbb44db9f4662440c433c
Emil Ajdyna <2088208+emilos@users.noreply.github.com>
2018-11-12 19:32:01 +0100

    Update dep

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const { compile } = require('@buxlabs/html-engine')
 const { getOptions } = require('loader-utils')
 
-module.exports = function (source) {
-  if (this.cacheable) this.cacheable()
+module.exports = async function (source) {
+  this.async()
+  this.cacheable()
   const options = Object.assign({ statistics: true }, getOptions(this))
   const transform = options.compile || compile
-  const { code, statistics } = transform(source, options)
-  if (typeof this.addDependency === 'function') {
-    statistics.assets.forEach(path => this.addDependency(path))
-  }
-
-  return `export default ${code.toString()}`
+  const { code, statistics, errors } = await transform(source, options)
+  statistics.assets.forEach(path => this.addDependency(path))
+  this.callback(errors[0], `export default ${code.toString()}`)
 }
 
--- test.js
+++ test.js
@@ -1,14 +1,32 @@
 const assert = require('assert')
 const loader = require('.')
-let source, output, paths = []
+const { spy } = require('sinon')
+let output, paths = []
 
-source = '<div>{foo}</div>'
-output = loader.call({ cacheable () {}, addDependency () {} }, source)
-assert.deepEqual(output, `export default function render(__o, __e) {
-var __t = "";
-__t += "<div";
-__t += ">";
-__t += __e(__o.foo);
-__t += "</div>";
-return __t;
-}`)
+function normalize (string) {
+  return string.replace(/\s+/g, '')
+}
+
+const callback = spy()
+const source = '<div>{foo}</div>'
+
+loader.call({
+  async () {},
+  callback,
+  cacheable () {},
+  addDependency () {}
+}, source).then(() => {
+  const output = callback.lastCall.args[1]
+  assert.deepEqual(normalize(output), normalize(
+    `
+    export default function render(__o, __e) {
+      var __t = "";
+      __t += "<div";
+      __t += ">";
+      __t += __e(__o.foo);
+      __t += "</div>";
+      return __t;
+    }
+    `
+  ))
+})
