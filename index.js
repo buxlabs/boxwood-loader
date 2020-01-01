e163e9d32a1487cde4d2311a4e2c1ab41fad0579
Emil Ajdyna <emil@ajdyna.com>
2020-01-01 11:35:50 +0100

    Add logging

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const { compile } = require('pure-engine')
 const { getOptions } = require('loader-utils')
+const { print } = require('./log')
 
 module.exports = async function (source) {
   this.async()
   this.cacheable()
   const options = Object.assign({ statistics: true }, getOptions(this))
   const transform = options.compile || compile
-  const { template, statistics, errors } = await transform(source, options)
+  const { template, statistics, warnings, errors } = await transform(source, options)
   statistics.assets.forEach(path => this.addDependency(path))
+  if (options.log) { print({ path: this.resourcePath, warnings, errors }) }
   this.callback(errors[0], `export default ${template.toString()}`)
 }
 
