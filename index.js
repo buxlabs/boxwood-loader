aead1286ba72ec15b3a520a008f2e9333e25b242
Emil Ajdyna <emil@ajdyna.com>
2020-01-01 11:43:02 +0100

    Add preprocess and postprocess options

--- index.js
+++ index.js
@@ -2,14 +2,21 @@ const { compile } = require('pure-engine')
 const { getOptions } = require('loader-utils')
 const { print } = require('./log')
 
-module.exports = async function (source) {
+module.exports = async function (input) {
   this.async()
   this.cacheable()
   const options = Object.assign({ statistics: true }, getOptions(this))
+  if (typeof options.preprocess === 'function') {
+    input = options.preprocess(input)
+  }
   const transform = options.compile || compile
-  const { template, statistics, warnings, errors } = await transform(source, options)
+  const { template, statistics, warnings, errors } = await transform(input, options)
   statistics.assets.forEach(path => this.addDependency(path))
   if (options.log) { print({ path: this.resourcePath, warnings, errors }) }
-  this.callback(errors[0], `export default ${template.toString()}`)
+  const output = template.toString()
+  if (typeof options.postprocess === 'function') {
+    output = options.postprocess(output)
+  }
+  this.callback(errors[0], `export default ${output}`)
 }
 
